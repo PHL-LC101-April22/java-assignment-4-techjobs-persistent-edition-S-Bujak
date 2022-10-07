23294a7038bd7c3a874f657ffc11a4be7b6cfb0d
S-Bujak <106119256+S-Bujak@users.noreply.github.com>
2022-10-07 15:44:34 -0400

    Part 2: Persisting Employers and Skills - Models and Data layer

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location cannot be blank.")
+    @Size(max=50, message="Location must have less than 50 characters.")
+    private String location;
 
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
